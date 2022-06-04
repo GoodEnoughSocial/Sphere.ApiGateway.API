40cf44d25ecd1dcaf97ea0d25b7e583a06ed7420
Chad Jessup <chad.jessup@gmail.com>
2022-06-04 00:21:51 -0700

    svc registration .econfig mods

--- Sphere.ApiGateway.API/Program.cs
+++ Sphere.ApiGateway.API/Program.cs
@@ -1,49 +1,60 @@
-using Consul;
-using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
+using Serilog;
+using Sphere.Shared;
 
-using var client = new ConsulClient();
+Log.Logger = SphericalLogger.SetupLogger();
 
-var registration = new AgentServiceRegistration
+Log.Information("Starting up");
+
+var registration = Services.ApiGateway.GetServiceRegistration();
+
+try
 {
-    ID = Guid.NewGuid().ToString(),
-    Name = "apigateway",
-    Port = 5003,
-    Address = "localhost",
-};
+    var result = await Services.RegisterService(registration);
 
-var result = await client.Agent.ServiceRegister(registration);
+    new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            config
+             .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+             .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+             .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+             .AddCommandLine(args)
+             .AddEnvironmentVariables();
+        })
+        .ConfigureServices(s =>
+        {
+            s.AddOcelot()
+            .AddConsul()
+            .AddConfigStoredInConsul();
+        })
+        .ConfigureLogging((hostingContext, logging) =>
+        {
+            logging.AddConsole();
+        })
+        .Configure(app =>
+        {
+            app.UseOcelot().Wait();
+        })
+        .Build()
+        .Run();
+}
+catch (Exception ex)
+{
+    if (ex.GetType().Name != "StopTheHostException")
+    {
+        Log.Fatal(ex, "Unhandled exception");
+    }
+}
+finally
+{
+    await Services.UnregisterService(registration);
 
-new WebHostBuilder()
-    .UseKestrel()
-    .UseContentRoot(Directory.GetCurrentDirectory())
-    .ConfigureAppConfiguration((hostingContext, config) =>
-    {
-        config
-         .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-         .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-         .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-         .AddCommandLine(args)
-         .AddEnvironmentVariables();
-    })
-    .ConfigureServices(s =>
-    {
-        s.AddOcelot()
-        .AddConsul()
-        .AddConfigStoredInConsul();
-    })
-    .ConfigureLogging((hostingContext, logging) =>
-    {
-        logging.AddConsole();
-    })
-    .Configure(app =>
-    {
-        app.UseOcelot().Wait();
-    })
-    .Build()
-    .Run();
-
-await client.Agent.ServiceDeregister(registration.ID);
+    Log.Information("Shutting down");
+    Log.CloseAndFlush();
+}
